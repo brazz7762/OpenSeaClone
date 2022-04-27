50f27dd85a06efd3f5e71a4c5bba1e52d9d50929
brazz7762 <rasmussen.benjaminj@gmail.com>
2022-04-27 17:29:11 -0500

    Added updates to the config file for hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
-
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").stoString();
+const projectId = "d2db1d720fb547d399039ac1af0083e1"
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 // task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -17,5 +19,16 @@ require("@nomiclabs/hardhat-waffle");
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  networks: {
+    hardhat:{
+      chainId: 1337;
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`
+    }
+  }
   solidity: "0.8.4",
 };
