b2dc8271c6775b4c495b9f4942e00631f69dfa08
brazz7762 <rasmussen.benjaminj@gmail.com>
2022-04-27 19:36:43 -0500

    Adding sol files with some initial work laid down

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,2 +1,24 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.7.0;
+
+import "@openzeppelin/contracts/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+  //auto-incrementing field for each token
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  //address of the NFT marketplace
+  address contractAddress;
+
+  constructor(address marketplaceAddress) ERC721("Salutokens", "SALT"){
+    contractAddress = marketplaceAddress;
+  }
+
+  function createToken(string memory tokenURI) public returns (uint){
+    return 23;
+  }
+
+}
