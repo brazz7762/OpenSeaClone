7c1b8e42f8e9e913ae8e5959d2badc8036d38e1f
brazz7762 <rasmussen.benjaminj@gmail.com>
2022-05-09 22:14:18 -0500

    Added NFT and Marketplace contracts to index file, plus code to link front end react and contract codes, added calls to get data

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,46 @@ import Web3Modal from 'web3modal';
 import { nftAddress, nftMarketAddress } from '../config.js';
 
 
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json';
+import NFTMarketplace from '../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json';
 
 export default function Home() {
+  const [nfts,setNFTS] = useState([]);
+  const [loadingState, setLoadingState] = useState('not-loaded');
+
+  useEffect(() => {
+    loadNFTs();
+
+  }, []);
+
+  async function loadNFTs(){
+    const provider = new ethers.providers.JsonRpcProvider();
+    const tokenContract = new ethers.Contract(nftAddress, NFT.abi, provider);
+    const mpContract = new ethers.Contract(nftMarketAddress, NFTMarketplace.abi, provider);
+    //gets array of not sold market items
+    const data = await mpContract.fetchMarketItems();
+    const items = await Promise.all(data.map(async i => {
+      const tokenUri = await tokenContract.tokenUri(i.tokenId);
+      const meta = await axios.get(tokenUri);
+      let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
+      let item = {
+        price,
+        tokenId: i.tokenId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image,
+        name: meta.data.name,
+        description: meta.data.description,
+      }
+      return item;
+    }));
+
+      setNFTs(items);
+
+      setLoadingState('loaded');
+
+  }
+
   return (
     <div className={styles.container}>
       <h1>HOM3 PAG3</h1>
